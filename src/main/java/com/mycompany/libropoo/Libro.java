4904348c07fcc170adfc2cba1e863da57e7df7c0
diego <hola@gmail.com>
2024-04-25 09:00:55 -0500

    Se realiza ejercicio pero falta correccion

--- src/main/java/com/mycompany/libropoo/Libro.java
+++ src/main/java/com/mycompany/libropoo/Libro.java
@@ -1,5 +1,37 @@
 package com.mycompany.libropoo;
 
 public class Libro {
-    
+private String a;    
+private String t;    
+private String g;    
+private String ap;    
+boolean l;
+
+public Libro (String a, String t, String g, String ap, int l){
+this.t = t;
+this.a = a;
+this.g = g;
+this.ap = ap;
+this.l = false;
 }
+
+    Libro(String t, String a, String g, int ap) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+public String mostraInformacion(){
+return "Titulo: "+ t + ", Autor: " + a + ", Año de publicación: " + 
+        ap + ", Género: " + g;
+}
+public void marcarLeido(){
+this.l=true;           
+}        
+public boolean antiguo(){        
+int ac = java.time.Year.now().getValue();
+ int aP = Integer.parseInt(ap);
+return (ac - aP)> 50;
+}
+public boolean Leido(){
+return l;
+}       
+}
+
--- src/main/java/com/mycompany/libropoo/Principal.java
+++ src/main/java/com/mycompany/libropoo/Principal.java
@@ -1,5 +1,29 @@
 package com.mycompany.libropoo;
 
+import java.util.Scanner;
 public class Principal {
-    
+public static void main(String[] args){
+Scanner sc= new Scanner (System.in);
+
+    System.out.println("Ingrese el título: ");
+    String t = sc.nextLine();
+    System.out.println("Ingrese el autor del libro: ");
+    String a = sc.nextLine();
+    System.out.println("Ingrese el año de publicación: ");
+    int ap = sc.nextInt();
+    System.out.println("Ingrese el género del libro: ");
+    String g= sc.nextLine();
+
+    Libro libro1 = new Libro(t,a,g,ap);
+    System.out.println(libro1.mostraInformacion());
+    libro1.marcarLeido();
+    System.out.println(libro1.antiguo());
+    boolean esLeido = libro1.Leido();
+    if(esLeido == true ){
+        System.out.println("El libro ya fue leido");
+        }
+    else{
+        System.out.println("El libro no ha sido leido");
+    }
+  }
 }
