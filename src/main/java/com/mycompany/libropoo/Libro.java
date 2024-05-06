cd55f30cedc30b485ab0d383521c64f6507a3078
Ander <ander@ander.com>
2024-05-06 08:44:47 -0500

    Se realiza corrección

--- src/main/java/com/mycompany/libropoo/Libro.java
+++ src/main/java/com/mycompany/libropoo/Libro.java
@@ -1,37 +1,34 @@
 package com.mycompany.libropoo;
-
 public class Libro {
-private String a;    
-private String t;    
-private String g;    
-private String ap;    
-boolean l;
+    private String a;    
+    private String t;    
+    private String g;    
+    private int ap; // Corregido el tipo de dato
+    boolean l;
 
-public Libro (String a, String t, String g, String ap, int l){
-this.t = t;
-this.a = a;
-this.g = g;
-this.ap = ap;
-this.l = false;
-}
-
-    Libro(String t, String a, String g, int ap) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public Libro(String a, String t, String g, int ap) {
+        this.t = t;
+        this.a = a;
+        this.g = g;
+        this.ap = ap;
+        this.l = false;
     }
-public String mostraInformacion(){
-return "Titulo: "+ t + ", Autor: " + a + ", Año de publicación: " + 
-        ap + ", Género: " + g;
-}
-public void marcarLeido(){
-this.l=true;           
-}        
-public boolean antiguo(){        
-int ac = java.time.Year.now().getValue();
- int aP = Integer.parseInt(ap);
-return (ac - aP)> 50;
-}
-public boolean Leido(){
-return l;
-}       
-}
 
+    public String mostraInformacion() {
+        return "Titulo: " + t + ", Autor: " + a + ", Año de publicación: " + 
+                ap + ", Género: " + g;
+    }
+
+    public void marcarLeido() {
+        this.l = true;           
+    }        
+
+    public boolean antiguo() {        
+        int ac = java.time.Year.now().getValue();
+        return (ac - ap) > 50;
+    }
+
+    public boolean Leido() {
+        return l;
+    }       
+}
